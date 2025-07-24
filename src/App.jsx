b1bf4fdc5c1df86e219c585e4bac1ae09661d4bd
sravanthimusuluri <sravanthimusuluri1910@gmail.com>
2025-07-24 20:02:26 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Front end development frameworks</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
